a8b1977411bf248f241e871116f3b2956a0c3025
Pedro Cavalcante <pedrodanilo48@gmail.com>
2022-12-09 14:55:46 -0300

    finalização

--- script.js
+++ script.js
@@ -4,26 +4,41 @@ document.querySelector('.busca').addEventListener('submit', async (evento)=>{
     let entrada = document.querySelector('#buscarEntrada').value;
 
     if(entrada !== '') {
+        limparInfo();
         mostrarAviso('Carregando...');
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(entrada)}&appid=7dd671ef277600273e84d5038e5fc419&units=metric&lang=pt_br`;
         let resultado = await fetch(url);
         let json = await resultado.json();
 
         if(json.cod === 200) {
-            showInfo({
-                name: json.name,
-                country: json.sys.country,
-                temp: json.main.temp,
-                tempIcon: json.weather[0].icon
-            })
+            mostrarInfo({
+                nome: json.name,
+                pais: json.sys.country,
+                temperatura: json.main.temp,
+                temperaturaIcone: json.weather[0].icon,
+                velocidadeVento: json.wind.speed,
+                anguloVento: json.wind.deg
+            });
         }else{
-            showWarning("Não encontrado");
+            limparInfo("");
+            mostrarAviso("Não encontrado");
         }
     }
 });
-function showInfo(json){
-    name: json.name,
-    country: json.sys
+function mostrarInfo(json){
+    mostrarAviso('');
+    
+    document.querySelector(".titulo").innerHTML = `${json.nome}, ${json.pais}`;
+    document.querySelector(".tempInfo").innerHTML = `${json.temperatura}<sup>ºC</sup>`;
+    document.querySelector(".ventoInfo").innerHTML = `${json.velocidadeVento}<span>km/h</span>`;
+    document.querySelector(".temp img").setAttribute('src', `http://openweathermap.org/img/wn/${json.temperaturaIcone}@2x.png`);
+    document.querySelector(".ventoPonto").style.transform = `rotate(${json.anguloVento-90}deg)`;
+
+    document.querySelector('.resultado').style.display = 'block';
+}
+function limparInfo() {
+    mostrarAviso('');
+    document.querySelector('.resultado').style.display = 'none';
 }
 function mostrarAviso(msg){
     document.querySelector('.aviso').innerHTML = msg;
